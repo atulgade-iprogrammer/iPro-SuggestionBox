066203df5fa09e1b16e45cd001cb2a0b02dfe61e
atulgade-iprogrammer <atulg@iprogrammer.co>
2023-05-30 11:12:40 +0530

    app.js commited

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const router = require("../router/router");
+const path = require("path");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// listening on Port 8000
+app.listen(8000);
+app.use(router);
+const htmlPath = path.join(__dirname, "../public");
+console.log(htmlPath);
+app.use(express.static(htmlPath));
+app.get("/", (req, res) => {
+  res.sendFile(htmlPath + "/index.html");
+});
